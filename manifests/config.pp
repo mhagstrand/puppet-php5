93eae7491cb89e5a07a0d57225346bd08b6a2dd8
Chris Boulton <chris@bigcommerce.com>
2014-11-20 14:31:10 -0800

    use puppetlabs/inifile to manage PHP configuration

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,17 +1,10 @@
 define php5::config(
   $value,
-  $target = '/etc/php5/conf.d/zzz_common.ini',
+  $target  = '/etc/php5/conf.d/zzz_common.ini',
   $section = '',
   $setting = '',
-  $ensure = 'present'
+  $ensure  = 'present'
 ) {
-  if ($section) {
-    $use_section = $section
-  }
-  else {
-    $use_section = '.anon'
-  }
-
   if ($setting) {
     $use_setting = $setting
   }
@@ -19,18 +12,11 @@ define php5::config(
     $use_setting = $name
   }
 
-  $full_target = "files${target}/${use_section}"
-
-  if ($ensure == 'absent') {
-    $use_command = 'rm'
-  }
-  else {
-    $use_command = 'set'
-  }
-
-  augeas { "php-config-${target}-${name}":
-    context => $full_target,
-    changes => "${use_command} ${use_setting} ${value}",
-    require => Package['php5-common'],
+  ini_setting { "php-config-${target}-${name}":
+    ensure  => $ensure,
+    section => $section,
+    setting => $use_setting,
+    value   => $value,
+    path    => $target,
   }
 }
