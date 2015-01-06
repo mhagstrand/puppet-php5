5a26fc6bdafda4122ccdc238e0d825bdb2b36ba8
mitchell.hagstrand <mitchell.hagstrand@bigcommerce.com>
2015-01-06 14:56:33 -0800

    Moving logrotate out of app server module

--- manifests/fpm/pool.pp
+++ manifests/fpm/pool.pp
@@ -1,9 +1,10 @@
 define php5::fpm::pool($prefix = '', $listen = '127.0.0.1', $port = '9000',
   $user = 'www-data', $group = 'www-data', $mode = '0666', $pm = 'dynamic',
   $pm_max_children = 50, $pm_start_servers = 20, $pm_min_spare = 5, $pm_max_spare = 35,
-  $pm_max_requests = 0, $env = [], $php_values = [], $php_admin_values = [], $access_log = '',
+  $pm_max_requests = 0, $env = [], $php_values = [], $php_admin_values = [], $access_log_file = '',
   $access_format = '', $misc_options = [], $ensure = 'present', $status_path = '/fpm-status',
   $ping_path = '/fpm-ping') {
+
     file { "/etc/php5/fpm/pool.d/${name}.conf":
       content   => template('php5/fpm-pool.conf.erb'),
       owner     => root,
@@ -12,17 +13,5 @@ define php5::fpm::pool($prefix = '', $listen = '127.0.0.1', $port = '9000',
       require   => File['/etc/php5/fpm/pool.d/'],
       notify    => Exec['reload-php5-fpm'],
       ensure    => $ensure,
-    }
-    # Make sure directory for access_log exists if access_log is set
-    # Will not create the entire directory tree
-    if (!empty($access_log)) {
-      $access_log_path = regsubst($access_log, '[^\/]*$', '')
-      file { "log_path_${name}":
-        path      => $access_log_path,
-        ensure    => 'directory',
-        owner     => root,
-        group     => root,
-        mode      => '750',
-      }
     }
 }
